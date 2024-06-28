48d303841b7c731826d8725975d258f7b3dca13a
Igor <igoralbieri04@gmail.com>
2024-06-28 00:17:21 -0300

    asda

--- includes/menu.php
+++ includes/menu.php
@@ -1,71 +1,120 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>cadastro_perfil</title>
-  <link rel="stylesheet" href="estilo_perfil.css">
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https/cdnjs.cloudflare.comlibs/font-awesome/6.4.0/css/all.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="../assets/css/menu.css">
-  <link rel="stylesheet" href="../assets/js/menu.js">
-
-</head>
-
+<!-- Created by CodingLab |www.youtube.com/CodingLabYT-->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../assets/css/menu.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+     
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
 <body>
-
-  <section class="main-menu">
-
-    <div class="container-menu">
-
-      <a href="#" class="toggleBox-menu">
-        <span class="icon-menu"></span>
-      </a>
-
-      
-
-
-
-      <ul class="navItems-menu">
-
-        <li class="li-menu">
-          <a href="main_home.php" class="link-menu">
-            <i class="bi bi-house-door" style="--i:2"></i>
-          </a>
-        </li>
-
-
-        <li class="li-menu">
-          <a href="main_cadastrar_cliente.php" class="link-menu">
-            <i class="bi bi-person" style="--i:3"></i>
-          </a>
-        </li>
-
-
-        <li class="li-menu">
-          <a href="main_cadastrar_lavagem.php" class="link-menu">
-            <i class="bi bi-water"></i>
-          </a>
-        </li>
-
-      </ul>
+  <div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus icon'></i>
+        <div class="logo_name">Lava Jato</div>
+        <i class='bx bx-menu' id="btn" ></i>
     </div>
-  </section>
-
+    <ul class="nav-list">
+      <li>
+          <i class='bx bx-search' ></i>
+         <input type="text" placeholder="Search...">
+         <span class="tooltip">Search</span>
+      </li>
+      <li>
+        <a href="main_home.php">
+          <i class="bi bi-house"></i>
+          <span class="links_name">Home</span>
+        </a>
+         <span class="tooltip">Home</span>
+      </li>
+      <li>
+       <a href="main_cadastrar_cliente.php">
+         <i class='bx bx-user' ></i>
+         <span class="links_name">Cadastrar Cliente</span>
+       </a>
+       <span class="tooltip">Cadastrar Cliente</span>
+     </li>
+     <li>
+       <a href="main_cadastrar_lavagem.php">
+        <i class="bi bi-water"></i>
+         <span class="links_name">Cadastrar Lavagem</span>
+       </a>
+       <span class="tooltip">Cadastrar Lavagem</span>
+     </li>
+     <li>
+       <a href="#">
+          <i class="bi bi-grid"></i>
+         <span class="links_name">Gerenciar Cliente</span>
+       </a>
+       <span class="tooltip">Gerenciar Cliente</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-folder' ></i>
+         <span class="links_name">File Manager</span>
+       </a>
+       <span class="tooltip">Files</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-cart-alt' ></i>
+         <span class="links_name">Order</span>
+       </a>
+       <span class="tooltip">Order</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-heart' ></i>
+         <span class="links_name">Saved</span>
+       </a>
+       <span class="tooltip">Saved</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-cog' ></i>
+         <span class="links_name">Setting</span>
+       </a>
+       <span class="tooltip">Setting</span>
+     </li>
+     <li class="profile">
+         <div class="profile-details">
+           <img src="profile.jpg" alt="profileImg">
+           <div class="name_job">
+             <div class="name">Prem Shahi</div>
+             <div class="job">Web designer</div>
+           </div>
+         </div>
+         <i class='bx bx-log-out' id="log_out" ></i>
+     </li>
+    </ul>
+  </div>
   <script>
-    var toggleClick = document.querySelector(".toggleBox-menu");
-    var container = document.querySelector(".container-menu");
-    toggleClick.addEventListener("click", () => {
-      toggleClick.classList.toggle("active");
-      container.classList.toggle("active");
-      submenu.classList.remove('active');
+  let sidebar = document.querySelector(".sidebar");
+  let closeBtn = document.querySelector("#btn");
+  let searchBtn = document.querySelector(".bx-search");
 
-    })
+  closeBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("open");
+    menuBtnChange();//calling the function(optional)
+  });
 
+  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
+  });
+
+  // following are the code to change sidebar button(optional)
+  function menuBtnChange() {
+   if(sidebar.classList.contains("open")){
+     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   }else {
+     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   }
+  }
   </script>
 </body>
-
 </html>
